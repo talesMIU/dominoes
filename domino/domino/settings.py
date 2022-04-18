65ed42dfe4f15dc056068d36e7cc2d4d09f9cec8
TalesMiura <talessmiura@gmail.com>
2022-04-18 05:10:40 -0300

    conect to mysql

--- domino/domino/settings.py
+++ domino/domino/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'domino.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'domino',
+        'USER': 'admin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
